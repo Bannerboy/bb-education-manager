46a1a1fbbfb5fdf9e30febef278bdd167c4bfeda
Elliot Sverin (Personal) <elliot.sverin@hyperisland.se>
2020-03-25 17:42:17 +0100

    Improved styling on coursecard and added more properties to it

--- src/components/courseList/CourseEntry.js
+++ src/components/courseList/CourseEntry.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react"
+import React, { Component } from "react"
 import styled from "styled-components";
 import PropTypes from 'prop-types';
 import {variables} from "../global/variables"
@@ -21,7 +21,7 @@ class CourseEntry extends Component{
             height: auto;
             background-color: ${variables.colorLightBlue};
             font-weight: 400;
-            transition: opacity 0.3 ease-in-out;
+            transition: opacity 0.3s ease-in-out;
             opacity: 1;
             color: ${variables.colorBlack};
             border-bottom: 0.05rem solid ${variables.colorWhite}; 
@@ -31,6 +31,25 @@ class CourseEntry extends Component{
                 padding: 0 2rem;
                 & .card {
                     display: flex;
+                    font-size: 1.5rem;
+                    & .tags {
+                        display: flex;
+                        justify-content: center;
+                        flex-direction: column;
+                    }
+                    & .tag {
+                        /* width: inherit; */
+                        text-align: center;
+                        border-radius: 1rem;
+                        padding: 0 .5rem;
+                        color: ${variables.colorWhite}
+                    }
+                    & .category {
+                        background-color: ${variables.colorRed};
+                    }
+                    & .difficulty {
+                        background-color: ${variables.colorDarkBlue}
+                    }
                 }
                 & .previewCard {
                     padding: 2rem 0;
@@ -39,7 +58,7 @@ class CourseEntry extends Component{
                 }
                 & .expandedCard {
                     transition: max-height 0.7s cubic-bezier(0.4, 0, 0.2, 1);
-                    padding: 0 0;
+                    padding: ${this.state.clicked ? "2rem 0rem" : "0 0"};
                     justify-content: space-between;
                     flex-direction: row;
                     max-height: ${this.state.clicked ? "10rem" : "0rem"};
@@ -62,8 +81,18 @@ class CourseEntry extends Component{
                 <ul>
                     <li>
                         <ul className="previewCard card" onClick={this.expandCard}>
-                            <li className="courseTitle">{this.props.course.title}</li>
-                            <li>{this.props.course.platform}</li>
+                            <li>
+                                <ul>
+                                    <li className="courseTitle">{this.props.course.title}</li>
+                                    <li>{this.props.course.platform}</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <ul className="tags">
+                                    <li className="tag category">{this.props.course.tag}</li>
+                                    <li className="tag difficulty">{this.props.course.difficulty}</li>
+                                </ul>
+                            </li>
                         </ul>
                     </li>
                     <li>
@@ -86,7 +115,10 @@ CourseEntry.defaultProps = {
     course: {
         title: "Course Title",
         author: "Author Name",
-        platform: "TutorialSite"
+        tag: "Motion Design",
+        platform: "Tutorial Site",
+        difficulty: "Easy",
+        url: "https://example.com",
     },
     };
 
